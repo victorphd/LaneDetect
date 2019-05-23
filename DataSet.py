7bb0a5b0a31499605eed29c64d10d4640aceecf9
victorphd <42841310+victorphd@users.noreply.github.com>
2019-05-23 01:10:43 -0700

    Add files via upload

---
+++ DataSet.py
@@ -0,0 +1,118 @@
+import sys
+import pickle
+import cv2
+import os, fnmatch
+import glob
+import sys
+import gc
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+def GetPickleDataSet():
+    # Load training images
+    train_images = pickle.load(open("X.p", "rb" ))
+
+    # Load image labels
+    labels = pickle.load(open("Y.p", "rb" ))
+
+    # Make into arrays as the neural network wants these
+    train_images = np.array(train_images)
+    labels = np.array(labels)
+
+    # Normalize labels - training images get normalized to start in the network
+    labels = labels/4
+
+    # Shuffle images along with their labels, then split into training/validation sets
+    train_images, labels = shuffle(train_images, labels)
+    # Test size may be 10% or 20%
+    X_train, X_val, y_train, y_val = train_test_split(train_images, labels, test_size=0.1)
+    
+    return (X_train, y_train, X_val, y_val)
+
+
+X_train_files = []
+Y_train_files = []
+X_dev_files = []
+Y_dev_files = []
+train_set_count = 0
+dev_set_count = 0
+
+def ScanFolders(dev_set_ratio):
+    global X_train_files
+    global Y_train_files
+    global X_dev_files
+    global Y_dev_files
+    global train_set_count
+    global dev_set_count
+
+    #X_top_folder = 'D:/cs230-project/CULaneOriginalImage'
+    #Y_top_folder = 'D:/cs230-project/CULaneLabels'
+   X_top_folder = '/Users/victor/Desktop/CULaneDetection/Data/CULaneOriginalImage'
+    Y_top_folder = '/Users/victor/Desktop/CULaneDetection/Data/CULaneLabels' 
+    
+    for subDir in os.listdir(X_top_folder):
+        X_path = os.path.join(X_top_folder, subDir)
+        Y_path = os.path.join(Y_top_folder, subDir)
+        if not os.path.isdir(X_path):
+                continue
+        for subDir2 in os.listdir(X_path):
+            X_path2 = os.path.join(X_path, subDir2)
+            Y_path2 = os.path.join(Y_path, subDir2)
+            if not os.path.isdir(X_path2):
+                continue
+            X_files = []
+            Y_files = []
+            for file in os.listdir(X_path2):
+                if fnmatch.fnmatch(file, "*.jpg"):
+                    X_img_file = os.path.join(X_path2, file)
+                    Y_img_file = os.path.join(Y_path2, file)
+                    Y_img_file = os.path.splitext(Y_img_file)[0] + '.png'
+                    if (os.path.isfile(Y_img_file)):
+                        X_files.append(X_img_file)
+                        Y_files.append(Y_img_file)
+
+            X, Y = shuffle(np.array(X_files), np.array(Y_files))
+            X_train, X_dev, Y_train, Y_dev = train_test_split(X, Y, test_size=dev_set_ratio)
+            X_train_files = X_train_files + X_train.tolist()            
+            Y_train_files = Y_train_files + Y_train.tolist()
+            X_dev_files = X_dev_files + X_dev.tolist()
+            Y_dev_files = Y_dev_files + Y_dev.tolist()
+        
+    train_set_count = len(X_train_files)
+    dev_set_count = len(X_dev_files)
+    print('Train Set:', str(train_set_count), 'Dev Set:', str(dev_set_count))
+
+
+
+def TrainDataGenerator(batch_size, mode):
+        
+    if mode == 'dev':
+        X_files = X_dev_files
+        Y_files = Y_dev_files
+    else:
+        X_files = X_train_files
+        Y_files = Y_train_files
+    
+    #print('Available images in', mode, 'dataset:', str(len(X_files)))
+    
+    ii = 0
+    X_train = []
+    Y_train = []
+    batch_count = 0
+    while True:                
+        imgArrY = cv2.imread(Y_files[ii], cv2.IMREAD_COLOR)
+        imgArrX = cv2.imread(X_files[ii], cv2.IMREAD_COLOR)
+        X_train.append(imgArrX)
+        Y_train.append(imgArrY)
+        if len(X_train) == batch_size:            
+            yield((np.array(X_train), np.array(Y_train)))
+            X_train = []
+            Y_train = []
+        
+        ii = ii + 1
+
+        if ii == len(X_files):
+            ii = 0
+        
+ScanFolders(0.1)
--- FindLaneInAnImage.py
+++ FindLaneInAnImage.py
@@ -2,36 +2,60 @@ import numpy as np
 from keras.models import load_model
 import cv2
 import matplotlib.pyplot as plt
-import os
+import os, sys, datetime
+import DataSet
+from PIL import Image
+
+
+
+def ApplyLaneDetection(Y_hat, XtestImg):
+    for i in range(Y_hat.shape[0]):
+        for j in range(Y_hat.shape[1]):
+            rvalue = int(Y_hat[i][j][0])
+            if (rvalue>=1):
+                XtestImg[i][j][rvalue%3] = 255
+    
+def SaveInFolder(id, XtestImg, Y, out_dir):
+    im = Image.fromarray(XtestImg)
+    imgFile = os.path.join(out_dir, str(id)+'.jpg')
+    im.save(imgFile)
+
+    im = Image.fromarray(Y)
+    imgFile = os.path.join(out_dir, str(id)+'_Y.jpg')
+    im.save(imgFile)
+    #cv2.imshow("image", XtestImg)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
 currentDir = os.path.dirname(os.path.realpath(__file__))
 model = load_model(os.path.join(currentDir, 'Model.h5'))
 
-XtestImgFile = os.path.join(currentDir, "01560.jpg")
-XtestImg = cv2.imread(XtestImgFile, cv2.IMREAD_COLOR)
+mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+os.makedirs(mydir)
 
-XList = np.array([ XtestImg ])
 
-print(XList.shape)
+#ds = DataSet.TrainDataGenerator(DataSet.dev_set_count, 'dev')
+ds = DataSet.TrainDataGenerator(10, 'dev')
+
+devSet = next(ds)
+XList = devSet[0]
+YList = devSet[1]
 Y_hat = model.predict(XList)
 
-Y_hat = Y_hat[0] * 4
+h = XList[0].shape[0] // 2
+w = XList[0].shape[1] // 2
+hw = ' height=' + str(h) + " width=" + str(w)
 
-print(Y_hat.shape[0], Y_hat.shape[1])
+htmlfile = os.path.join(mydir, 'index.html')
+out = open(htmlfile, 'w')
+out.write('<html><body><table>')
+for ii in range(XList.shape[0]):
+    tempX = np.copy(XList[ii])
+    ApplyLaneDetection(Y_hat[ii], XList[ii])
+    ApplyLaneDetection(YList[ii], tempX)
+    SaveInFolder(ii, XList[ii], tempX, mydir)
+    out.write('<tr><td><img src=\'' + str(ii) + '.jpg\'' + hw + '></td><td><img src=\'' + str(ii)+'_Y.jpg\'' + hw + '></td></tr>')
 
-for i in range(Y_hat.shape[0]):
-    for j in range(Y_hat.shape[1]):
-        if (Y_hat[i][j][0]>=1):
-            XtestImg[i][j][0] = 0
-            XtestImg[i][j][1] = 255
-            XtestImg[i][j][2] = 0
-            
+out.write('</table></body></html>')
+out.close()
 
-#plt.figure('image')
-#plt.imshow(XtestImg)
-
-#plt.show()
-
-cv2.imshow("image", XtestImg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
